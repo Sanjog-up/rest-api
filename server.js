30d7f26958e907387bf365a4dd1e5edc6fa4a1dd
Sanjog Aale <sanjogm940@gmail.com>
2026-05-03 18:19:48 +0545

    rest api

--- server.js
+++ server.js
@@ -47,6 +47,17 @@ app.get("/products", (req, res) => {
     size: 10,
   });
 });
+//? get by id
+// /users/1
+app.get("/products/:id", (req, res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  res.json({
+    _id: id,
+    name: "Pant",
+    size: 10,
+  });
+});
 
 app.post("/products", (req, res) => {
   res.json({
@@ -76,3 +87,67 @@ server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log("press ctrl+c to close server");
 });
+
+//! REST API 
+//? REST -> REPRESENTATIONAL STATE TRANSFER
+//? API -> Application Programming Interface  
+
+//* client-server
+
+//* stateless
+//  R1 -> U1 -> RE1
+//  R2 -> U1 ->
+
+//* res cacheable
+
+// * layered system
+
+// c -> proxy server -> load balancer -> server
+
+// ! resource => //{}
+//! endpoint =>   /users/orders/1
+//! route => app.get("/products", () => {})
+//! representation => json
+
+//!  uses standard http req methods 
+//* GET -> get req/ query
+//* POST -> create new data
+//* PATH -> update  
+//* PUT -> update data 
+//* DELETE -> remove resource
+
+//! uses standard http response status code
+//* 100 -599
+//? 100-199 -> informational res
+//? 200-299 -> successful res
+//? 300-399 -> redirectional res
+//? 400-499 -> client side error
+//? 500-599 -> server side  error
+
+// * uniform interface
+//get-user
+
+// endpoint 
+//? resource collections
+// /users
+// /products 
+
+// single resource
+// /users/1
+
+//! parameters
+// heep://localhost:8080/products?name=shirt&size=10&page=1&limit=5
+//* search/query params => req.quqery => {name:'',email:'',page:'',limit:''}
+
+//* route params =>
+//  /users/1 => /users/:id =>  req.params => {id:1}
+//  /users/1/orders/1 => /users/:id/orders/:order:id =>  req.params => {id:1}
+//* /users/100 => req.params => {xyz:100}
+
+
+
+
+//! stateless -> each req is independent of other reqs
+//! cacheable -> response can be cached by client or intermediary servers
+//! layered system -> client doesn't know if it's connected directly to the end server or an intermediary server    
+//! uniform interface -> standardized way of communication between client and server
